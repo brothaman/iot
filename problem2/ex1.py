d2140a21a8dc0aaf4e0dca3d487710044fa4bfcc
Ubuntu <ubuntu@ip-172-31-22-95.us-east-2.compute.internal>
2018-03-10 14:53:28 +0000

    tested ex1 beginning ex2

--- problem2/ex1.py
+++ problem2/ex1.py
@@ -3,11 +3,11 @@ from mrjob.step import MRStep
 
 class MRmyjob(MRJob):
 	def mapper(self, _, line):
-	    agram,bgram,year,count,page,num_books = line.split(' ')
-            yield [agram,bgram], count
+		agram,bgram,year,count,page,num_books = line.split()
+		yield [agram,bgram], int(count)
 
 	def reducer(self, bigram, count):
 		yield None, (bigram, sum(count))
 
 if __name__ == '__main__':
-    MRmyjob.run()
+	MRmyjob.run()
