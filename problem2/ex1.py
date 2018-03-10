6a9ac5c59cd7702b8c198f8eab29e0bfbe22795f
brothaman <rbrothers12@gmail.com>
2018-03-10 01:27:20 -0600

    testing exercise 1

---
+++ problem2/ex1.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class MRmyjob(MRJob):
+	def mapper(self, _, line):
+	    agram,bgram,year,count,page,num_books = line.split(' ')
+            yield [agram,bgram], count
+
+	def reducer(self, bigram, count):
+		yield None, (bigram, sum(count))
+
+if __name__ == '__main__':
+    MRmyjob.run()
